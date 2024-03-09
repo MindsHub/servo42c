8854b15ccb8ebe10ae78d8b789ea52e2448fa376
Stypox <stypox@pm.me>
2024-03-09 11:24:12 +0100

    Add base implementation for state handler

---
+++ backend/src/state/mod.rs
@@ -0,0 +1,49 @@
+use std::{sync::{Arc, Mutex, MutexGuard}, thread, time::Duration};
+
+#[derive(Debug)]
+struct State {
+    target_x: f64,
+    target_y: f64,
+    target_z: f64,
+    x: f64,
+    y: f64,
+    z: f64,
+    water: bool,
+}
+
+#[derive(Clone)]
+struct StateHandler {
+    state: Arc<Mutex<State>>,
+    // TODO add serial object
+}
+
+fn acquire(state: &Arc<Mutex<State>>) -> MutexGuard<'_, State> {
+    match state.lock() {
+        Ok(guard) => guard,
+        Err(poisoned) => poisoned.into_inner(),
+    }
+}
+
+macro_rules! mutate_state {
+    ($state:expr, $($field:ident = $value:expr),+) => {
+        {
+            let mut state = acquire($state);
+            $(state.$field = $value;)*
+        }
+    };
+}
+
+impl StateHandler {
+    fn move_to(&self, x: f64, y: f64, z: f64) {
+        // TODO send command to Arduino
+        mutate_state!(&self.state, target_x = x, target_y = y, target_z = z);
+    }
+
+    fn water(&self, duration: Duration) {
+        // TODO send command to Arduino to turn on water
+        mutate_state!(&self.state, water = true);
+        thread::sleep(duration);
+        // TODO send command to Arduino to turn off water
+        mutate_state!(&self.state, water = false);
+    }
+}
